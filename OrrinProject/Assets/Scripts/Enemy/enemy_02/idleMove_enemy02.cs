012988e2c835c4157bd2c96cb09da4c88d3ba461
kvppn <128691178+kvppn@users.noreply.github.com>
2025-03-17 21:50:07 +0800

    怪5

---
+++ OrrinProject/Assets/Scripts/Enemy/enemy_02/idleMove_enemy02.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+public class idleMove_enemy02 : SpiritualEnemyAction
+{
+    public SharedVector3 currentTarget;
+    public float moveSpeed = 1f;
+
+    public SharedGameObject targetGameObject;
+    public SharedString stateName;
+    public int layer = -1;
+    public float normalizedTime = float.NegativeInfinity;
+    // Start is called before the first frame update
+    public override void OnStart()
+    {
+        var currentGameObject = GetDefaultGameObject(targetGameObject.Value);
+            animator = currentGameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    public override TaskStatus OnUpdate()
+    {
+        // 计算移动方向
+        Vector3 direction = (currentTarget.Value - transform.position).normalized;
+        animator.Play(stateName.Value, layer, normalizedTime);
+        // 根据移动方向改变敌人的朝向
+        UpdateFacingDirection(direction);
+        // 移动敌人
+        transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
+
+        // 检查是否到达目标点
+        if (Vector3.Distance(transform.position, currentTarget.Value) < 1f)
+        {
+            return TaskStatus.Success; // 到达目标点，返回成功
+        }
+        return TaskStatus.Running; // 仍在移动中
+    }
+    private void UpdateFacingDirection(Vector3 direction)
+    {
+        
+        // 如果移动方向的 x 分量大于 0，敌人面向右
+        if (direction.x > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        // 如果移动方向的 x 分量小于 0，敌人面向左
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
