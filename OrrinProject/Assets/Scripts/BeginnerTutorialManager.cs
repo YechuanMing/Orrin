5b0820b866e53e47eca0e0f563004533b0fb8f80
kvppn <128691178+kvppn@users.noreply.github.com>
2025-04-13 13:46:10 +0800

    TMPMESHPRO中文字体前

---
+++ OrrinProject/Assets/Scripts/BeginnerTutorialManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginnerTutorialManager : MonoBehaviour
+{
+    //void Start()
+    //{
+    //    //游戏初始的值
+    //    PlayerPrefs.SetInt("intGemi", 1);//双胞胎的对话
+    //    PlayerPrefs.SetInt("intbar", 1);//跟酒店老板娘的对话
+    //    PlayerPrefs.SetInt("ChestOpened_1", 0);//表示宝箱已经被打开
+    //    PlayerPrefs.SetInt("intKey", 0);//这是开始计时时间的触发，初始为0
+    //    PlayerPrefs.SetInt("intFlag", 3);
+    //    for (int i = 0; i <= 40; i++)
+    //    {
+    //        PlayerPrefs.SetInt("DataInitialized" + "tile_" + i.ToString(), 0);//判断耕地是否初始
+
+    //    }
+
+    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
