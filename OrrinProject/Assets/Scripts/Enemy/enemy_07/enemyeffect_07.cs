0e4d8c0917acb5d04565e7fe01a7ad0ebdf7a8bb
kvppn <128691178+kvppn@users.noreply.github.com>
2025-04-17 22:15:40 +0800

    敌人7 ok

--- OrrinProject/Assets/Scripts/Enemy/enemy_07/enemyeffect_07.cs
+++ OrrinProject/Assets/Scripts/Enemy/enemy_07/enemyeffect_07.cs
@@ -6,7 +6,6 @@ public class enemyeffect_07 : SpiritualEnemyAction
 {
     public GameObject bombPref;
     public float radius = 4f;
-    public float angleOffset = 30f; // 角度偏移（单位：度）
     public override TaskStatus OnUpdate()
     {
 
@@ -15,7 +14,7 @@ public class enemyeffect_07 : SpiritualEnemyAction
             float angle = Mathf.Atan2(from.y, from.x) * Mathf.Rad2Deg; // 用于旋转
             float signedAngle = Vector2.SignedAngle(Vector2.right, from); // 有符号夹角
             // Step 3: 计算生成位置
-            Vector3 spawnPos = transform.position + from * radius;
+            Vector3 spawnPos = player.transform.position - from * radius;
             // Step 4: 让 b 朝向它的飞行方向
             // Step 4: 计算旋转，让炸弹朝向玩家方向
             Quaternion rotation = Quaternion.Euler(0, 0, angle);
