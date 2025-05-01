a4da3cdb858a51abfef1a5b948f2edf5119bc775
kvppn <128691178+kvppn@users.noreply.github.com>
2025-05-01 18:50:54 +0800

    灵魂依次摧毁

--- OrrinProject/Assets/Scripts/Enemy/BOSS/SoulsBeDestroyInStage.cs
+++ OrrinProject/Assets/Scripts/Enemy/BOSS/SoulsBeDestroyInStage.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class SoulsBeDestroyInStage : MonoBehaviour
 {
+    public Destructable destructable;
+    public int firstBeDestroyHealth;
+    public int secondBeDestroyHealth;
+    public int thirdBeDestroyHealth;
+    public GameObject firstSoul;
+    public GameObject secondSoul;
+    public GameObject thirdSoul;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,20 @@ public class SoulsBeDestroyInStage : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Bedestroy()
+    {
+        if (destructable.currHealth <= thirdBeDestroyHealth)
+        {
+            Destroy(thirdSoul, 1f);
+        }
+        else if (destructable.currHealth <= secondBeDestroyHealth)
+        {
+            Destroy(secondSoul, 1f);
+        }
+        else if (destructable.currHealth <= firstBeDestroyHealth)
+        {
+            Destroy(firstSoul, 1f);
+        }
     }
 }
