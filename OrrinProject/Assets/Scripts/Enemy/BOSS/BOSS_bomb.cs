80faab0d97e6df882c2f8eea824200fa2b317bd8
kvppn <128691178+kvppn@users.noreply.github.com>
2025-05-02 21:03:05 +0800

    BOSS

--- OrrinProject/Assets/Scripts/Enemy/BOSS/BOSS_bomb.cs
+++ OrrinProject/Assets/Scripts/Enemy/BOSS/BOSS_bomb.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class BOSS_bomb : MonoBehaviour
 {
-
-  
+    public GameObject Bomb;
     public void bomb()
     {
-        
+        Vector3 center = transform.position;
+        Instantiate(Bomb, center, Quaternion.identity);
     }
 }
