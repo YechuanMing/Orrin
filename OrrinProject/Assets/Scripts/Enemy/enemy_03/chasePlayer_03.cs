9e86cc8cd3a699f6ca2107500f4e1ab9928b7ac7
kvppn <128691178+kvppn@users.noreply.github.com>
2025-05-03 21:51:52 +0800

    怪3

--- OrrinProject/Assets/Scripts/Enemy/enemy_03/chasePlayer_03.cs
+++ OrrinProject/Assets/Scripts/Enemy/enemy_03/chasePlayer_03.cs
@@ -7,45 +7,47 @@ using System.Linq;
 using DG.Tweening;
 public class chasePlayer_03 : PhysicEnemyAction
 {
-    public float jumpDuration = 1f; // 跳跃持续时间
     public float jumpForce = 4f;//跳跃速度
     public float buildupTime;//起跳时间
     public float horizontalForce;//水平力度
-
-    private bool haslanded;
-
+    public bool jumpStarted = false;
+    private bool haslanded = false;
     private Tween buildUpTween;
     private Tween jumpTween;
+
     public override void OnStart()
     {
         Debug.Log(player);
-        buildUpTween =DOVirtual.DelayedCall(buildupTime, StartJump, false);
-        animator.Play("Jump");
+        buildUpTween =DOVirtual.DelayedCall(buildupTime, StartJump, false);//可能没啥用
+        animator.Play("jumping");
     }
-
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, LayerMask.GetMask("Ground"));
+        return hit.collider != null;
+    }
     private void StartJump()
     {
         var direction = PlayerController.Instance.transform.position.x < transform.position.x ? -1 : 1;
     
         rigidbody.AddForce(new Vector2(horizontalForce * direction, jumpForce), ForceMode2D.Impulse);
-
-        jumpTween=DOVirtual.DelayedCall(jumpDuration, () => { haslanded = true; }, false);
-    }
-    float GetAnimationLength(string animationName)
-    {
-        AnimationClip clip = animator.runtimeAnimatorController.animationClips.FirstOrDefault(c => c.name == animationName);
-        return clip != null ? clip.length : 0f;
+        jumpStarted = true;
+        jumpTween=DOVirtual.DelayedCall(0.2f, () => { haslanded = true; }, false);
     }
     public override TaskStatus OnUpdate()
     {
-
-        return haslanded ? TaskStatus.Success : TaskStatus.Running;
+        if (IsGrounded()&&jumpStarted&&haslanded)
+        {
+           return TaskStatus.Success;
+        } 
+        return TaskStatus.Running;
     }
 
     public override void OnEnd()
     {
         buildUpTween.Kill();
         jumpTween.Kill();
+        jumpStarted = false;
         haslanded = false;
     }
 
