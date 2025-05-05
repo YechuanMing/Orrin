9ab0347bda9f42b6a721a73ca0b12ef93c6eb707
kvppn <128691178+kvppn@users.noreply.github.com>
2025-05-05 22:51:25 +0800

    boss

--- OrrinProject/Assets/Scripts/Enemy/BOSS/SoulsBeDestroyInStage.cs
+++ OrrinProject/Assets/Scripts/Enemy/BOSS/SoulsBeDestroyInStage.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SoulsBeDestroyInStage : MonoBehaviour
 {
     public int BeDestroyNum = 0;
-    
+    public bool shocked1=false;
+    public bool shocked2 = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,14 @@ public class SoulsBeDestroyInStage : MonoBehaviour
     public void Bedestroy()
     {
         BeDestroyNum++;
+        if (BeDestroyNum == 1)
+        {
+            shocked1 = true;
+        }
+        if (BeDestroyNum == 2)
+        {
+            shocked2 = true;
+        }
         if (BeDestroyNum == 3)
         {
             transform.GetComponent<SpiritualEnemyBase>().isSpiritDied = true;
--- OrrinProject/Assets/Scripts/Enemy/BOSS/checkandshocked.cs
+++ OrrinProject/Assets/Scripts/Enemy/BOSS/checkandshocked.cs
@@ -1,11 +1,12 @@
+using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class checkandshocked : PhysicEnemyAction
+public class checkandshocked : Conditional
 {
-    public int num;//灵魂状态下的血量
+    //public int num;//灵魂状态下的血量
     public bool firstBeShocked = false;
     public bool secondBeShocked = false;
     // Start is called before the first frame update
@@ -17,15 +18,16 @@ public class checkandshocked : PhysicEnemyAction
     // Update is called once per frame
     public override TaskStatus OnUpdate()
     {
-        num = transform.GetComponent<SoulsBeDestroyInStage>().BeDestroyNum;
-        if (num==2 && !secondBeShocked)
+        if (gameObject.transform.GetComponent<SoulsBeDestroyInStage>().shocked1 && !firstBeShocked)
         {
-            secondBeShocked = true;
+            Debug.Log("shocked1 成立，打断执行！");
+            firstBeShocked = true;
             return TaskStatus.Success;
         }
-        else if(num == 1 && !firstBeShocked)
+        else if(gameObject.transform.GetComponent<SoulsBeDestroyInStage>().shocked2 && !secondBeShocked)
         {
-            firstBeShocked = true;
+            Debug.Log("shocked2 成立，打断执行！");
+            secondBeShocked = true;
             return TaskStatus.Success;
         }
         return TaskStatus.Failure;
