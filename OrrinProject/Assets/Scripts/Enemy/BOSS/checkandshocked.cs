b4a62304e29f3345266c34198675c2cf5a8a6da3
kvppn <128691178+kvppn@users.noreply.github.com>
2025-05-05 21:25:00 +0800

    改一下值方便测试

--- OrrinProject/Assets/Scripts/Enemy/BOSS/checkandshocked.cs
+++ OrrinProject/Assets/Scripts/Enemy/BOSS/checkandshocked.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class checkandshocked : PhysicEnemyAction
 {
-    private int num;//灵魂状态下的血量
-    private bool firstBeShocked = false;
-    private bool secondBeShocked = false;
+    public int num;//灵魂状态下的血量
+    public bool firstBeShocked = false;
+    public bool secondBeShocked = false;
     // Start is called before the first frame update
     public override void  OnStart()
     {
