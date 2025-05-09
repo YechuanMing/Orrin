713d2e3d0e3ede993b8c4e2802da0d0677fbe551
kvppn <128691178+kvppn@users.noreply.github.com>
2025-05-09 15:09:53 +0800

    boss二阶段

---
+++ OrrinProject/Assets/Scripts/Enemy/BOSS/BT/spirit2mask2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+public class spirit2mask2 : Conditional
+{
+    public override TaskStatus OnUpdate()
+    {
+        if (transform.GetComponent<spirit2WhichStateMask>().shocked1 && !transform.GetComponent<spirit2WhichStateMask>().shocked2)
+            return TaskStatus.Success;
+        
+        return TaskStatus.Failure;
+    }
+}
