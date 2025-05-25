5932b6dfe4b617c59764611651b4e449c195ea96
Msqqqqqqq <msq187163566@gmail.com>
2025-05-25 21:31:20 +0800

    1

--- OrrinProject/Assets/Scripts/Money.cs
+++ OrrinProject/Assets/Scripts/Money.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Money : MonoBehaviour
 {
     public int value=1;
+    public AudioClip[] pickSounds;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             GameManager.Instance.playerDataObj.wealth += value;
             PlayerDisplayData.Instance.UpdateMoneyNum();
+            AudioManager.Instance.PlaySoundEffect(pickSounds[Random.Range(0, pickSounds.Length - 1)]);
             Destroy(gameObject);
+
         }
     }
 }
